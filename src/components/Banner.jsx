33772d56919737ac6a4a7a4ca2e9dbb588500b31
Akanksha Dubey <akankshadubey2558@gmail.com>
2024-09-26 22:19:01 +0530

    commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -137,6 +137,76 @@
 // export default Banner;
 
 
+// import { useEffect, useState } from "react";
+// import { useMovieContext } from "../context/MovieContext";
+// import Card from "./Card";
+
+// const Banner = () => {
+//   const [backgroundImage, setBackgroundImage] = useState("");
+//   const [searchQuery, setSearchQuery] = useState("");
+//   const { fetchByQuery, searchResults, popular } = useMovieContext();
+
+//   useEffect(() => {
+//     if (popular.length > 0) {
+//       const randomImage =
+//         popular[Math.floor(Math.random() * popular.length)].backdrop_path;
+//       setBackgroundImage(`https://image.tmdb.org/t/p/original${randomImage}`);
+//     }
+//   }, [popular]);
+
+//   const handleSearch = (e) => {
+//     e.preventDefault();
+//     if (searchQuery.trim()) {
+//       fetchByQuery(searchQuery);
+//     }
+//   };
+
+//   return (
+//     <div
+//       className="min-h-[70vh] flex flex-col items-center justify-center bg-cover bg-center p-4"
+//       style={{ backgroundImage: `url(${backgroundImage})` }}
+//     >
+//       <h1 className="text-9xl font-bold mb-2 text-white">Welcome</h1>
+//       <br />
+//       <p className="text-lg font-medium mb-4 text-white">
+//         Millions of movies, TV shows, and people to discover. Explore now.
+//       </p>
+//       <form onSubmit={handleSearch} className="flex items-center">
+//         <input
+//           type="text"
+//           placeholder="Search for a movie or TV show..."
+//           className="px-20 py-4 rounded-l-full shadow-md w-96 placeholder:text-center focus:outline-none focus:ring-2 focus:ring-blue-500"
+//           value={searchQuery}
+//           onChange={(e) => setSearchQuery(e.target.value)}
+//         />
+//        <button
+//     type="submit"
+//     className="px-8 py-4 text-white font-bold rounded-r-full"
+//     style={{
+//         background: 'linear-gradient(98.37deg, #f89e00 .99%, #da2f68 100%)'
+//     }}
+// >
+//     Search
+// </button>
+//       </form>
+
+//       {searchResults.length > 0 && (
+//         <div className="w-full bg-white bg-opacity-70 p-4 rounded-lg shadow-lg mt-4">
+//           <h2 className="text-2xl font-bold mb-2">Search Results:</h2>
+//           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+//             {searchResults.map((item) => (
+//               <Card key={item.id} movie={item} />
+//             ))}
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default Banner;
+
+
 import { useEffect, useState } from "react";
 import { useMovieContext } from "../context/MovieContext";
 import Card from "./Card";
@@ -167,7 +237,6 @@ const Banner = () => {
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <h1 className="text-9xl font-bold mb-2 text-white">Welcome</h1>
-      <br />
       <p className="text-lg font-medium mb-4 text-white">
         Millions of movies, TV shows, and people to discover. Explore now.
       </p>
@@ -179,15 +248,15 @@ const Banner = () => {
           value={searchQuery}
           onChange={(e) => setSearchQuery(e.target.value)}
         />
-       <button
-    type="submit"
-    className="px-8 py-4 text-white font-bold rounded-r-full"
-    style={{
-        background: 'linear-gradient(98.37deg, #f89e00 .99%, #da2f68 100%)'
-    }}
->
-    Search
-</button>
+        <button
+          type="submit"
+          className="px-8 py-4 text-white font-bold rounded-r-full"
+          style={{
+            background: 'linear-gradient(98.37deg, #f89e00 .99%, #da2f68 100%)'
+          }}
+        >
+          Search
+        </button>
       </form>
 
       {searchResults.length > 0 && (
@@ -195,7 +264,7 @@ const Banner = () => {
           <h2 className="text-2xl font-bold mb-2">Search Results:</h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
             {searchResults.map((item) => (
-              <Card key={item.id} movie={item} />
+              <Card key={item.id} item={item} mediaType={item.media_type} /> 
             ))}
           </div>
         </div>
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import  { useState, useRef, useEffect } from 'react';
 import Card from './Card';
-import ToggleButton from './ToggleButton';
+// import ToggleButton from './ToggleButton';
 
 const Carousel = ({ title, movies }) => {
     const [currentView, setCurrentView] = useState('Day');
@@ -37,49 +37,87 @@ const Carousel = ({ title, movies }) => {
     );
 
     return (
+        // <section className="my-12 px-4 relative">
+        //     <div className="flex justify-between items-center mb-6">
+        //         <h2 className="text-3xl font-bold text-white">{title}</h2>
+        //         {/* <ToggleButton 
+        //             options={['Day', 'Week']} 
+        //             defaultOption="Day" 
+        //             onChange={(option) => setCurrentView(option)}
+        //         /> */}
+        //     </div>
+        //     <h2 className="text-3xl font-bold mb-6 text-gray-800 text-white ml-4">{title}</h2>
+        //     <div className="relative">
+        //         <ArrowButton
+        //             direction="left"
+        //             onClick={() => handleScroll(-1)}
+        //             disabled={scrollPosition === 0}
+        //         />
+        //         <div
+        //             className="overflow-hidden"
+        //             ref={carouselRef}
+        //         >
+        //             <div 
+        //                 className="flex space-x-4 transition-transform duration-300 ease-out"
+        //                 style={{ transform: `translateX(-${scrollPosition}px)` }}
+        //             >
+        //                 {/* {movies.map((movie) => (
+        //                     <div className="flex-none w-64" key={movie.id}>
+        //                         <Card movie={movie} />
+        //                     </div>
+        //                 ))} */}
+        //                  {movies.map((movie) => (
+        //             <Card key={movie.id} item={movie} mediaType={movie.media_type || 'movie'} />
+        //         ))}
+        //             </div>
+        //         </div>
+        //         {scrollPosition < maxScroll && (
+        //             <ArrowButton
+        //                 direction="right"
+        //                 onClick={() => handleScroll(1)}
+        //                 disabled={scrollPosition >= maxScroll}
+        //             />
+        //         )}
+        //     </div>
+        // </section>
         <section className="my-12 px-4 relative">
-            <div className="flex justify-between items-center mb-6">
-                <h2 className="text-3xl font-bold text-white">{title}</h2>
-                <ToggleButton 
-                    options={['Day', 'Week']} 
-                    defaultOption="Day" 
-                    onChange={(option) => setCurrentView(option)}
-                />
-            </div>
-            <h2 className="text-3xl font-bold mb-6 text-gray-800 text-white ml-4">{title}</h2>
-            <div className="relative">
-                <ArrowButton
-                    direction="left"
-                    onClick={() => handleScroll(-1)}
-                    disabled={scrollPosition === 0}
-                />
-                <div
-                    className="overflow-hidden"
-                    ref={carouselRef}
-                >
-                    <div 
-                        className="flex space-x-4 transition-transform duration-300 ease-out"
-                        style={{ transform: `translateX(-${scrollPosition}px)` }}
-                    >
-                        {/* {movies.map((movie) => (
-                            <div className="flex-none w-64" key={movie.id}>
-                                <Card movie={movie} />
-                            </div>
-                        ))} */}
-                         {movies.map((movie) => (
+    <div className="flex justify-between items-center mb-6">
+        <h2 className="text-3xl font-bold text-white">{title}</h2>
+        {/* <ToggleButton 
+            options={['Day', 'Week']} 
+            defaultOption="Day" 
+            onChange={(option) => setCurrentView(option)}
+        /> */}
+    </div>
+    <div className="relative">
+        <ArrowButton
+            direction="left"
+            onClick={() => handleScroll(-1)}
+            disabled={scrollPosition === 0}
+        />
+        <div
+            className="overflow-hidden"
+            ref={carouselRef}
+        >
+            <div 
+                className="flex space-x-4 transition-transform duration-300 ease-out"
+                style={{ transform: `translateX(-${scrollPosition}px)` }}
+            >
+                {movies.map((movie) => (
                     <Card key={movie.id} item={movie} mediaType={movie.media_type || 'movie'} />
                 ))}
-                    </div>
-                </div>
-                {scrollPosition < maxScroll && (
-                    <ArrowButton
-                        direction="right"
-                        onClick={() => handleScroll(1)}
-                        disabled={scrollPosition >= maxScroll}
-                    />
-                )}
             </div>
-        </section>
+        </div>
+        {scrollPosition < maxScroll && (
+            <ArrowButton
+                direction="right"
+                onClick={() => handleScroll(1)}
+                disabled={scrollPosition >= maxScroll}
+            />
+        )}
+    </div>
+</section>
+
     );
 };
 
